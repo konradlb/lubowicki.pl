affdd3928ae0c524f4f1780653ee103f6db3f628
Konrad Lubowicki <konrad@lubowicki.pl>
2026-04-11 13:01:51 +0200

    refactored i18n

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,5 +1,12 @@
 import { ui, defaultLang } from './ui';
 
+export function getI18nPaths() {
+  return [
+    { params: { lang: undefined } },
+    { params: { lang: 'en' } },
+  ];
+}
+
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof typeof ui[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
